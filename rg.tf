693c34763f1a56c71204dfa6fe78132a487e55a9
Mehtab_Qureshi <mq3274@email.com>
2024-11-12 22:02:50 +0530

    hp rg added

--- rg.tf
+++ rg.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "example1" {
   name     = "rahul"
   location = "Central india"
 }
+
+resource "azurerm_resource_group" "example1" {
+  name     = "hp-rg"
+  location = "West UD"
+}
