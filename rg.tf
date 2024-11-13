d0ba8c3c49bc381cf776611a730a8b2c4d656e97
Mehtab_Qureshi <mq3274@email.com>
2024-11-13 13:00:19 +0530

    removed 2 rg

--- rg.tf
+++ rg.tf
@@ -1,12 +1,3 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-1"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "example1" {
-  name     = "rg-2"
-  location = "East US"
-}
 
 resource "azurerm_resource_group" "example1" {
   name     = "rahul"
